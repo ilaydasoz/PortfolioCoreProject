1bb3a0eb12de71e43a8f5b07ff06d035c1e6f212
ilaydasiz <you@example.com>
2022-11-23 02:22:25 +0300

    contact and message parts added

---
+++ DataAccessLayer/Migrations/20221122224027_MailColumnAdded.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class MailColumnAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Mail",
+                table: "Contacts",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mail",
+                table: "Contacts");
+        }
+    }
+}
